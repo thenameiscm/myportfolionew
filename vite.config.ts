3d05f6d25a0e06bc37129336bc9a43258bf1e43a
Chandra mouli <141259145+thenameiscm@users.noreply.github.com>
2025-11-17 18:28:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+   base: '/myportfolionew/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
